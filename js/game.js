1786166a00c20ac95f09ba5b228a8108d612904c
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-05-30 14:33:25 -0400

    Added sounds, play different sounds for each color

--- js/game.js
+++ js/game.js
@@ -8,7 +8,8 @@ function nextSequence() {
 
   $("#" + randomColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  console.log(randomColor);
+  let audio = new Audio("sounds/" + randomColor + ".mp3");
+  audio.play();
 }
 
 nextSequence();
